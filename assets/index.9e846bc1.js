e34665c5443d50ee5996d269d28bb1bf6f5df736
quangmv-rikkeisoft <quangmv@rikkeisoft.com>
2023-10-04 13:45:58 +0700

    Updates

--- assets/index.9e846bc1.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./Meta.849397c2.js";import{F as a,T as t}from"./App.0bac4dea.js";import{B as i,F as o,j as r}from"./index.d0054462.js";import"./index.module.92ed0f1f.js";import"./index.5ad769ac.js";function d(){return i(o,{children:[r(e,{title:"page 2"}),r(a,{children:r(t,{variant:"h3",children:"Page 2"})})]})}export{d as default};

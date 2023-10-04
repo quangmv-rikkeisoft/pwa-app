e8dec156fd32a41d3917b94a0c2be7596b0797ed
quangmv-rikkeisoft <quangmv@rikkeisoft.com>
2023-10-04 16:09:08 +0700

    Updates

--- assets/index.05e7b227.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./Meta.1a856b47.js";import{F as a,T as t}from"./App.3fdf477d.js";import{B as i,F as o,j as r}from"./index.d0054462.js";import"./index.module.92ed0f1f.js";import"./index.2831d98d.js";function d(){return i(o,{children:[r(e,{title:"page 1"}),r(a,{children:r(t,{variant:"h3",children:"Page 1"})})]})}export{d as default};
---
+++ assets/index.b6055f71.js
@@ -0,0 +1,1 @@
+import{M as e}from"./Meta.51954aac.js";import{F as a,T as t}from"./App.c698189a.js";import{B as i,F as o,j as r}from"./index.d0054462.js";import"./index.module.92ed0f1f.js";import"./index.f65c4422.js";function d(){return i(o,{children:[r(e,{title:"page 2"}),r(a,{children:r(t,{variant:"h3",children:"Page 2"})})]})}export{d as default};
